8c296061586a8d1ad5fd763d4d5338baaafd0655
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-09-08 22:08:12 +0900

    spring_kadai_formを追加しました

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,33 @@
+package com.example.springkadaiform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+
+@Controller
+public class ContactFormController {
+	
+	@GetMapping("/form")
+	public String contactForm(Model model) {
+		model.addAttribute("contactForm", new ContactForm());
+		return "contactFormView";
+	}
+	
+	@PostMapping("/confirm")
+	public String confirmView(Model model,
+			@Validated ContactForm contactForm, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return "contactFormView";
+		}
+		
+		model.addAttribute("contactForm", contactForm);
+		return "confirmView";
+	}
+	
+}
