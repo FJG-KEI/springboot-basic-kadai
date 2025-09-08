5f4743d11895f7fba68bd678f933a00cd493938e
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-09-08 22:59:50 +0900

    spring_kadai_formを修正しました

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,11 +1,13 @@
 package com.example.springkadaiform.controller;
 
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
@@ -14,19 +16,27 @@ public class ContactFormController {
 	
 	@GetMapping("/form")
 	public String contactForm(Model model) {
-		model.addAttribute("contactForm", new ContactForm());
+		
+		if(!model.containsAttribute("contactForm")) {
+			model.addAttribute("contactForm", new ContactForm());
+		}
+		
 		return "contactFormView";
 	}
 	
 	@PostMapping("/confirm")
-	public String confirmView(Model model,
+	public String confirmView(RedirectAttributes redirectAttributes,
 			@Validated ContactForm contactForm, BindingResult result) {
 		
 		if (result.hasErrors()) {
-			return "contactFormView";
+			redirectAttributes.addFlashAttribute("contactForm", contactForm);
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+					+ Conventions.getVariableName(contactForm), result);
+			
+			return "redirect:/form";
 		}
 		
-		model.addAttribute("contactForm", contactForm);
+		redirectAttributes.addFlashAttribute("contactForm", contactForm);
 		return "confirmView";
 	}
 	
